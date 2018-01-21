6c01b6923d7c11db12edfc635cdc772ef9cbfc85
Bryan Bell-Smith <bryan.bellsmith@gmail.com>
2018-01-21 10:03:25 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a RIMWorld mod, designed to allow players to change their pawns' traits over time
---
+++ Source/ICanChange/ICanChange/ThingDef_MeditationMAt.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace ICanChange
+{
+    public class ThingDef_MeditationMat : ThingDef
+    {
+
+    }
+}
