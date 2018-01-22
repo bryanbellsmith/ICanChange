b58d9c378c73f6642deb2c6c48e062c253ceeb84
Bryan Bell-Smith <bryan.bellsmith@gmail.com>
2018-01-21 20:15:29 -0500

    modified:   Defs/ThingDefs_Buildings/Buildings_Furniture.xml 	Changing the mat back into a bed, might be tricky, but I want to 	inherit ownership.
    	modified:   Defs/WorkTypeDefs/WorkTypes.xml
    	Fixed the link to intellectual skill so that meditation job won't be
    	blocked by traits.
    
    	modified:   Source/ICanChange/ICanChange/MeditationMat.cs
    	Switched back to bed type, work ongoing
    
    	new file:   Source/ICanChange/ICanChange/Pawn_Mat_Ownership.cs
    	Created a new Mat Ownership class based on Verse.Pawn_Ownership, but
    	it needs to be reworked	from scratch, so I want a record of the
    	previous version committed.

--- Source/ICanChange/ICanChange/MeditationMat.cs
+++ Source/ICanChange/ICanChange/MeditationMat.cs
@@ -7,8 +7,46 @@ using Verse;
 
 namespace ICanChange
 {
-    public class MeditationMat : ThingDef
+    public class MeditationMat : Building, IAssignableBuilding
     {
+        public List<Pawn> owners = new List<Pawn>();
 
+        public IEnumerable<Pawn> AssigningCandidates
+        {
+            get
+            {
+                if (!base.Spawned)
+                {
+                    return Enumerable.Empty<Pawn>();
+                }
+                return base.Map.mapPawns.FreeColonists;
+            }
+        }
+
+        public IEnumerable<Pawn> AssignedPawns
+        {
+            get
+            {
+                return this.owners;
+            }
+        }
+
+        public int MaxAssignedPawnsCount
+        {
+            get
+            {
+                return 1; // only one owner allowed per mat. Look into a better place to store this.
+            }
+        }
+
+        public void TryAssignPawn(Pawn owner)
+        {
+            
+        }
+
+        public void TryUnassignPawn(Pawn pawn)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Source/ICanChange/ICanChange/Pawn_Mat_Ownership.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+
+namespace ICanChange
+{
+    internal class Pawn_Mat_Ownership : IExposable
+    {
+        private Pawn pawn;
+
+        private MeditationMat intOwnedMat;
+
+        public Pawn_Mat_Ownership(Pawn pawn)
+        {
+            this.pawn = pawn;
+        }
+
+        public MeditationMat OwnedMat
+        {
+            get
+            {
+                return this.intOwnedMat;
+            }
+            private set
+            {
+                if (this.intOwnedMat != value)
+                {
+                    this.intOwnedMat = value;
+                }
+            }
+        }
+
+        public void ExposeData()
+        {
+            Scribe_References.Look<MeditationMat>(ref this.intOwnedMat, "ownedMat", false);
+            if (Scribe.mode == LoadSaveMode.PostLoadInit)
+            {
+                if (this.OwnedMat != null)
+                {
+                    this.OwnedMat.owners.Add(this.pawn);
+                   // this.OwnedMat.SortOwners(); // what does this do? do i need it? copied from Verse.PawnOwnership
+                }
+            }
+        }
+
+        public void ClaimMat(MeditationMat newMat)
+        {
+            if (newMat.owners.Contains(this.pawn))
+            {
+                return;
+            }
+            this.UnclaimMat();
+            newMat.owners.Add(this.pawn);
+            // removed a owner count check that was in Verse.PawnOwnership. mats should only have one owner
+            // newMat.SortOwners(); // what does this do? do i need it? copied from Verse.PawnOwnership
+            this.OwnedMat = newMat;
+        }
+
+        public void UnclaimMat()
+        {
+            if (this.OwnedMat != null)
+            {
+                this.OwnedMat.owners.Remove(this.pawn);
+                this.OwnedMat = null;
+            }
+        }
+
+    }
+
+}
